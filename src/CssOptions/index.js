5143e9eab5616616e7c31440f947d557fd385765
MuneerMek <mmekraz781@proton.me>
2023-12-28 19:33:19 -0500

    Rendered CssOptionsList Component

--- src/CssOptions/index.js
+++ src/CssOptions/index.js
@@ -1,13 +1,15 @@
 import React from "react";
-
 /*
   TODO: import and render the presentational components
   needed to finish decomposing our app into small react components,
   each of which is responsible for a small, simple part of the UI
 */
+import CssOptionsList from "./CssOptions/CssOptionsList.js";
+import SelectedCssOption from "./CssOptions/SelectedCssOption.js";
 
 const CssOptions = (props) => (
   <div>
+    <CssOptionsList selectColor={props.selectColor} />
     <h2>CSS Color Options</h2>
     <div className="color-options-row"></div>
   </div>
